9b82bd039c178f560ab0e20e8ef402aad41f57d1
andyagreda <49949572+andyagreda@users.noreply.github.com>
2019-06-07 18:16:47 -0400

    Automatizando Menu
    Implementacion de Arrays para y ciclo for para llenar la lista front end

--- src/app/componentes/menu/menu.page.ts
+++ src/app/componentes/menu/menu.page.ts
@@ -8,7 +8,22 @@ import {Router} from '@angular/router'
 })
 export class MenuPage implements OnInit {
 
-  constructor(public router : Router) { }
+  public empanadas: string[];
+  constructor(public router : Router) {
+    this.empanadas = [
+      'Pollo',
+      'Res',
+      'Mixto (pollo-res)',
+      'Mariscos',
+      'Fricase',
+      'Charque',
+      'Jamon y Queso',
+      'Pizza',
+      'Choclo con Queso',
+      'Queso',
+      'Dulce de Leche'
+    ];
+   }
 
   ngOnInit() {
   }
