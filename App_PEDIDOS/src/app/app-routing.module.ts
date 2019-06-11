07a8613927ff3dbf3455cc965e07ab0b3aa88e0a
eavtono7 <eavtono7@gmail.com>
2019-06-10 23:39:43 -0400

    Nuevas pages

--- App_PEDIDOS/src/app/app-routing.module.ts
+++ App_PEDIDOS/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'cliente', loadChildren: './cliente/cliente.module#ClientePageModule' },
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+  { path: 'pedido', loadChildren: './pedido/pedido.module#PedidoPageModule' },
 ];
 
 @NgModule({
