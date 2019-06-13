c4bdca8f43b62e8face0b01dcc26a756f5f9397d
FabriRomero555 <fabricioromero555@gmail.com>
2019-06-13 09:48:24 -0400

    ...

--- src/app/componentes/pedido/pedido.component.ts
+++ src/app/componentes/pedido/pedido.component.ts
@@ -7,6 +7,17 @@ import {NavParams, ModalController} from '@ionic/angular';
   styleUrls: ['./pedido.component.scss'],
 })
 export class PedidoComponent implements OnInit {
+  public detallesList = [];
+
+  public detalle : {
+
+    nombre_chacha : string,
+    cantidad_chacha : number,
+    precio_detalle : number,
+
+  }
+
+  
 
   constructor() { }
 
