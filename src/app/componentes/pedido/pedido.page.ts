1da4fb67cd88d4fdc88239d97bb3fab877500519
FabriRomero555 <fabricioromero555@gmail.com>
2019-06-17 23:49:44 -0400

    Correciones de navegabilidad

--- src/app/componentes/pedido/pedido.page.ts
+++ src/app/componentes/pedido/pedido.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-pedido',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoPage implements OnInit {
 
-  constructor() { }
+  constructor(public router : Router) { }
 
   ngOnInit() {
   }
 
+  IraConfirmar(){
+    this.router.navigate(['/confirmacion']);
+  }
+  IraMenu(){
+    this.router.navigate(['/menu-orden'])
+  }
 }
